d33ced6350c6abf9e24d67249afa181b8a89c44c
Eyrma <erotstein@outlook,com>
2023-10-31 19:41:53 -0700

    partial

--- public/index.js
+++ public/index.js
@@ -1,56 +1,97 @@
-let ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K',]
-let suits = ['♠', '♥', '♣', '♦']
-let allDecks = [];
 let game = [];
-let playerOneHand = $('#player-one');
-let playerTwoHand = $('#player-two');
-let casino = $('.casino');
+//let playerOneHand = $('#player-one');
+// let playerTwoHand = $('#player-two');
+//let casino = $('.casino');
 
+var dealerSum = 0;
+var playerOneHandSum = 0;
+var playerTwoHandSum = 0;
+
+var dealerAceCount = 0;
+var playerOneHandAceCount = 0;
+var playerTwoHandAceCount = 0;
+
+//keep track of the hidden cards
+var hidden;
+var allDecks;
+
+//allowing the player to draw while the player sum <= 21
+var playHit = true;
+
+window.onload = function () {
+    buildDeck();
+    shuffleDeck();
+    startGame();
+}
 
 function buildDeck() {
-    let deck = [];
-    for (let i = 0; i < ranks.length; i++)
-    {
-        for (let x = 0; x < suits.length; x++)
-        {
-            let weight = parseInt(ranks[i]);
-            if (ranks[i] == 'J' || ranks[i] == 'Q' || ranks[i] == 'K')
-                weight = 10;
-            if (ranks[i] == 'A');
-                weight = 11;
-            let card = { Rank: ranks[i], Suit: suits[x], weight: weight };
-            deck.push(card);
+    let ranks = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King',]
+    let suits = ['♠', '♥', '♣', '♦']
+    allDecks = [];
+
+    for (let i = 0; i < suits.length; i++) {
+        for (let x = 0; x < ranks.length; x++) {
+            allDecks.push(ranks[x] + "-" + suits[x]);
         }
-        return(deck);
     }
+    console.log(allDecks);
 }
 
-function shuffleDecks(num) {
-    for (let i = 0; i < num; i++){
-        let location1 = Math.floor((Math.random() * deck.length));
-        let location2 = Math.floor((Math.random() * deck.length));
-        let newDeck = allDecks[location1];
-        
-        allDecks[location1] = allDecks[location2];
-        allDecks[location2 = newDeck];
+function createPlayers(playerOneHand) {
+    playerOneHand = new Array();
+    for (let i = 1; i < playerOneHand; i++){
+        var hand = new Array();
+        var playerOneHand = { Name: 'Player' + i, ID: i, Points: 0, Hand: hand };
+        createPlayers.push(playerOneHand);
     }
-}
-
-function selectRandomCard() {
-    let randomIndex = Math.floor(Math.random() * allDecks.length);
-    let card = allDecks[randomIndex];
-    allDecks.splice(randomIndex, 1);
-    return card;
-}
     
-    
-function dealHands () {
-    dealerHand = [selectRandomCard(), selectRandomCard()];
-    dealerHand.forEach((card, index) => {
-        const newCard = 
-    })
+}
+function shuffleDeck() {
+    for (let i = 0; i < allDecks.length; i++){
+        let x = Math.floor((Math.random() * allDecks.length));
+        let temp = allDecks[i];
+        allDecks[i] = allDecks[x];
+        allDecks[x] = temp;
+    }
+    console.log(allDecks);
 }
 
 function startGame() {
-   document.getElementById('btnStart').value =
+    hidden = allDecks.pop();
+    dealerSum += getValue(hidden);
+
+    //counting the Ace
+    dealerAceCount += checkAce(hidden);
+    //console.log(hidden);
+    //console.log(dealerSum);
+    while (dealerSum < 17) {
+        //creating image tag
+        let dealerCards = document.createElement('img');
+        let card = allDecks.pop();
+        dealerCards.src = "./DeckofCards" + card + '.png';
+        dealerSum += getValue(card);
+        dealerAceCount += checkAce(card);
+        document.getElementById('dealer-cards').append(dealerCards);
+    }
+    console.log(dealerSum);
+}
+ 
+function getValue(card) {
+    let data = card.split("-");
+    let value = data[0];
+
+    if (isNaN(value)) {
+        if (value == "Ace") {
+            return 11;
+        }
+        return 10;
+    }
+    return parseInt(value);
+}
+//checking the Ace
+function checkAce(card) {
+    if (card[0] == 'Ace') {
+        return 1;
+    }
+    return 0;
 }
